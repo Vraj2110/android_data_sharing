1a52f498605111f3b01386cf47c0d2a279739c44
Vraj Desai <desaivraj18@gmail.com>
2026-06-26 13:32:19 +0530

    next will be compose

--- app/src/main/java/com/example/datasharing/MainActivity2.java
+++ app/src/main/java/com/example/datasharing/MainActivity2.java
@@ -1,6 +1,10 @@
 package com.example.datasharing;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,15 +16,30 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity2 extends AppCompatActivity {
 
     TextView tresult;
+    Button sharebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
         tresult = findViewById(R.id.txt1);
+        sharebtn = findViewById(R.id.sharebtn);
 
         String text = getIntent().getStringExtra("text");
+
+        // Show data in TextView
         tresult.setText(text);
+
+        // Share data
+        sharebtn.setOnClickListener(v -> {
+
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, text);
+
+            startActivity(Intent.createChooser(shareIntent, "Share using"));
+        });
     }
 }
